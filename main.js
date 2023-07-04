6ecf2ab1dde152fa38a9efc79247d9f3bae9212a
stefani22tms <120654348+stefani22tms@users.noreply.github.com>
2023-07-04 08:23:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// function init() {
-//     console.log('radi')
-// }
+function init() {
+    console.log('radi')
+}
 
-// function initChatBot(url, inactiveMessage) {
-//     console.log(url)
-//     init()
-// }
-
-console.log('radi')
+function initChatBot(url, inactiveMessage) {
+    console.log(url)
+    init()
+}
