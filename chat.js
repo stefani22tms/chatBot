fef1c68872284bbe34af551f0b27af2bc90b6824
stefani22tms <120654348+stefani22tms@users.noreply.github.com>
2023-07-04 10:20:37 +0200

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,7 +2,7 @@ function init() {
     const template = `
         <div id="chat-container">
             <button class='chat-btn'>
-                <img src = "assets/img/chatbot-avatar.png" class = "icon" >
+                X
             </button>
         </div>
     `
