1c8573e4aef0e75cf349f3cb959dc698238f92b3
stefani22tms <120654348+stefani22tms@users.noreply.github.com>
2023-07-21 13:01:45 +0200

    Add files via upload

---
+++ framet.js
@@ -0,0 +1,33 @@
+
+
+/* init */
+function init() {
+    console.log('init')
+    let chatContainer = document.createElement('div')
+    chatContainer.id = 'chat-container'
+    chatContainer.setAttribute('style', 'position: fixed;bottom: 0; right: 0; width: 100%; height: 100%; z-index: 99')
+
+    iframe = document.createElement('iframe')
+    iframe.src = 'https://master.dttig9r6d49oq.amplifyapp.com/'
+    iframe.name = 'trafftChatBotiFrame'
+    iframe.height = '100%'
+    iframe.width = '100%'
+    iframe.scrolling = 'no'
+    iframe.style.border = 'none'
+    iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms')
+
+    chatContainer.appendChild(iframe)
+    document.body.appendChild(chatContainer)
+}
+
+
+/* initialize chatBot */
+function initChatBot(url, inactiveMessage, greetMessage) {
+    window.onload = () => {
+      tenantUrl = url
+      inactiveMessage = inactiveMessage
+      greetMessage = greetMessage
+      init()
+    }
+}
+
